b85755bcb900cf01495aa0c3b79d745c1c4c2a02
Ivan Kravtsov <cold67@mail.ru>
2020-03-23 17:40:57 +0300

    proj skeleton ready

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Header from './components/SharedComponents/Header';
+import PropertySearchContainer from './components/PropertySearchPage/SearchContainer';
+import SearchResultsContainer from './components/SearchResultsPage/SearchResultsContainer';
+import ListingContainer from './components/ListingPage/ListingContainer';
+import FavouritesContainer from './components/FavouritesPage/FavouritesContainer';
+import ErrorPage from './components/ErrorPages/ErrorPage';
+
+export interface Props {}
+
+const Stack = createStackNavigator();
+
+const App: React.FC<Props> = props => {
+  return (
+    <NavigationContainer>
+      <Header />
+
+      <Stack.Navigator initialRouteName="PropertySearchPage">
+        <Stack.Screen
+          name="PropertySearchPage"
+          component={PropertySearchContainer}
+        />
+        <Stack.Screen
+          name="SearchResultsPage"
+          component={SearchResultsContainer}
+        />
+        <Stack.Screen name="ListingPage" component={ListingContainer} />
+        <Stack.Screen name="FavouritesPage" component={FavouritesContainer} />
+        <Stack.Screen name="ErrorPage" component={ErrorPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
