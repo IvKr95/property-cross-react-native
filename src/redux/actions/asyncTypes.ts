6cae4b46569397835ac5ab9fc5ed3b6f8d020aa4
Ivan Kravtsov <cold67@mail.ru>
2020-04-01 11:41:06 +0300

    Files removed

--- src/redux/actions/asyncTypes.ts
+++
@@ -1,7 +0,0 @@
-const getAsyncActions = (type: string) => ({
-  REQUEST: `${type}_REQUEST`,
-  SUCCESS: `${type}_SUCCESS`,
-  FAILURE: `${type}_FAILURE`,
-});
-
-export const SEARCH_LOCATION = getAsyncActions('SEARCH_LOCATION');
