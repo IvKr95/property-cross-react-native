269966da79bfa572d43d29d72015d1e477df47a5
IvKr95 <48466057+IvKr95@users.noreply.github.com>
2020-04-01 11:29:15 +0300

    Revert "homepage done"

--- src/redux/sagas/rootSaga.ts
+++
@@ -1,6 +0,0 @@
-import {all, call} from 'redux-saga/effects';
-import {watchGetGeolocation, watchSearchLocation} from './sagas';
-
-export default function* rootSaga() {
-  yield all([call(watchSearchLocation), call(watchGetGeolocation)]);
-}
